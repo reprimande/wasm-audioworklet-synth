8ca926af3d7d52eeb0233fb5481d22dd7855f45b
Naoki Nomoto <happygrind@gmail.com>
2018-12-21 10:13:45 +0900

    move

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ echo "compile wasm"
 cargo build --target wasm32-unknown-unknown --release
 
 echo "gc wasm"
-RUST_BACKTRACE=1 wasm-gc target/wasm32-unknown-unknown/release/wasm_audioworklet.wasm -o docs/wasm/wasm_audioworklet.wasm
+RUST_BACKTRACE=1 wasm-gc target/wasm32-unknown-unknown/release/wasm_audioworklet.wasm -o wasm/wasm_audioworklet.wasm
 
 echo "finish!"
